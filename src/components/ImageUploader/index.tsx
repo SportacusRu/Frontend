69218caa802d641c9dcfed5decad0045c78c534f
tarkue <mrfrostlikchannel@gmail.com>
2024-06-19 21:44:34 +0500

    add loader

--- src/components/ImageUploader/index.tsx
+++ src/components/ImageUploader/index.tsx
@@ -1,25 +1,31 @@
 "use client";
-import { useId } from "react";
+import { useId, useState } from "react";
 import Icon from "../Icon";
 import { Icons } from "../Icon/types";
 import { Colors } from "../color";
 import { ImageUploaderProps } from "./types";
 import { minimization } from "@/extensions/minimization";
+import Loader from "../Loader";
 
 export default function({onUpload}: ImageUploaderProps) {
     const inputId = useId()
+    const [loading, setLoading] = useState(false);
     const uploadHandler = async (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.files && onUpload) 
         {
+            setLoading(true)
             var minimizedImage = await minimization(e.target.files[0], {
                 quality: 0.5,
                 type: "image/png"
             })
-            if (minimizedImage) onUpload(minimizedImage)
+            if (minimizedImage) 
+                onUpload(minimizedImage);
+            setLoading(false);
         }
     }
     return (
         <div className="imageUploaderWrapper">
+            <Loader loading={loading} />
             <input type="file" accept="image/png, image/jpeg" id={inputId} onChange={uploadHandler} />
             <label htmlFor={inputId} className="imageUploader">
                 <div>
